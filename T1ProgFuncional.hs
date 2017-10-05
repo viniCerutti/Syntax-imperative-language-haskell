6d94b6aa5650be0bcf042a414f796f28d2e3804e
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-10-05 20:16:30 -0300

    Adicionado os arquivos do programa

---
+++ T1ProgFuncional.hs
@@ -0,0 +1,51 @@
+module T1ProgFuncional where
+
+import Store
+-- Data que guarda as operacoes aritmeticas
+data AritExp =  L Integer                | Sub AritExp AritExp |
+                Add AritExp AritExp      | Mult AritExp AritExp|
+                Div AritExp AritExp      | V Char  deriving (Show,Eq,Ord)
+            
+-- Data que guarda as operacoes logicas
+data BoolExp =  B Bool                | No BoolExp          |
+                And' BoolExp BoolExp  | Or' BoolExp BoolExp |
+                Great AritExp AritExp | Less AritExp AritExp|
+                Equal AritExp AritExp  deriving (Show,Eq,Ord)
+                
+-- Data que guarda os comandos para nossa lingugagem
+data Commands = Atrib Char AritExp                |
+                Seq Commands Commands             |
+                Choice BoolExp Commands Commands  |
+                Loop BoolExp Commands deriving (Show)
+
+memory = initial
+
+sto2 = update memory 'x' 4
+
+evalBoolExp::BoolExp -> Store -> Bool
+evalBoolExp (B val) _ = val -- testado
+evalBoolExp (No exp1) store =  not (evalBoolExp exp1 store) -- testado
+evalBoolExp (And' exp1 exp2) store = (evalBoolExp exp1 store) && (evalBoolExp exp2 store) -- testado
+evalBoolExp (Or' exp1 exp2) store = (evalBoolExp exp1 store) || (evalBoolExp exp2 store) -- testado
+evalBoolExp (Great exp1 exp2) store = (evalAritExp exp1 store) > (evalAritExp exp2 store)
+evalBoolExp (Less exp1 exp2) store = ( evalAritExp exp1 store) < (evalAritExp exp2 store)
+evalBoolExp (Equal exp1 exp2) store = ( evalAritExp exp1 store) == (evalAritExp exp2 store)
+
+evalAritExp:: AritExp-> Store -> Integer
+evalAritExp (L val) _ = val -- testado
+evalAritExp (V c) store = value store c -- testado
+evalAritExp (Sub exp1 exp2) store = (evalAritExp exp1 store) - (evalAritExp exp2 store) -- testado
+evalAritExp (Add exp1 exp2) store = (evalAritExp exp1 store) + (evalAritExp exp2 store) -- testado
+evalAritExp (Mult exp1 exp2) store = (evalAritExp exp1 store) * (evalAritExp exp2 store)
+evalAritExp (Div exp1 exp2) store = (evalAritExp exp1 store) `div` (evalAritExp exp2 store)
+
+-- Inicio de um programa
+atrib00 = Atrib 'f' (L 1)
+
+atrib00Loop = Great (V 'n') (L 0)
+atrib01Loop = Atrib 'f' (Mult(V 'f') (V 'n'))
+atrib02Loop = Atrib 'n' (Sub(V 'n') (L 1))
+loop = Loop atrib00Loop (Seq atrib01Loop atrib02Loop)
+
+prog = Seq atrib00 loop         
+
