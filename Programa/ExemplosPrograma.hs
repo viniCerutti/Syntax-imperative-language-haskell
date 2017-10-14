20895b7af30dd4cf2f07c5314cf16e1a5eff08e9
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-10-14 19:48:54 -0300

    Adicionado a função para calcular multiplicação e alterado os nomes dos atributos

--- Programa/ExemplosPrograma.hs
+++ Programa/ExemplosPrograma.hs
@@ -3,11 +3,10 @@ module ExemplosPrograma where
 import T1ProgFuncional
 import Store
 
-
-
 {-
 	Programa 01: Calculo do fatorial com apenas somas
-	em outra linguagem de programacao
+
+	Em outra linguagem de programacao
 
 	  int n, m; // n é o numero para se calcular o fatorial
 	  int r = 1;
@@ -26,24 +25,53 @@ import Store
 
 -}
 
-memoryProg1 = initial
+memoryFat = initial
 
 -- atribuicao inicial de n
-prog1AtribN = Atrib 'n' (L 4) -- numero que se deseja descobrir o fatorial
-prog1AtribM = Atrib 'm' (L 0) -- variavel auxiliar para realizar a somas sucessivas
-prog1AtribR = Atrib 'r' (L 1) -- variavel para guardar o resultado
-prog1BoolLoop1 = Great (V 'n') (L 0) -- expressao n > 0 do primeiro laco
-prog1Atrib1Loop1 = Atrib 'm' (V 'n') -- m = n
-prog1Atrib2Loop1 = Atrib 't' (L 0) -- t = 0
-prog1BoolLoop2 = Great (V 'm') (L 0) -- expressao m > 0 do segundo laco
-prog1Atrib1Loop2 = Atrib 't' (Add (V 't') (V 'r')) -- t = t + r
-prog1Atrib2Loop2 = Atrib 'm' (Sub (V 'm') (L 1)) -- m = m - 1
-prog1Atrib3Loop1 = Atrib 'r' (V 't') -- r = t
-prog1Atrib4Loop1 = Atrib 'n' (Sub (V 'n') (L 1)) -- n = n - 1
+fatAtribN = Atrib 'n' (L 4) -- numero que se deseja descobrir o fatorial
+fatAtribM = Atrib 'm' (L 0) -- variavel auxiliar para realizar a somas sucessivas
+fatAtribR = Atrib 'r' (L 1) -- variavel para guardar o resultado
+fatBoolLoop1 = Great (V 'n') (L 0) -- expressao n > 0 do primeiro laco
+fatAtrib1Loop1 = Atrib 'm' (V 'n') -- m = n
+fatAtrib2Loop1 = Atrib 't' (L 0) -- t = 0
+fatBoolLoop2 = Great (V 'm') (L 0) -- expressao m > 0 do segundo laco
+fatAtrib1Loop2 = Atrib 't' (Add (V 't') (V 'r')) -- t = t + r
+fatAtrib2Loop2 = Atrib 'm' (Sub (V 'm') (L 1)) -- m = m - 1
+fatAtrib3Loop1 = Atrib 'r' (V 't') -- r = t
+fatAtrib4Loop1 = Atrib 'n' (Sub (V 'n') (L 1)) -- n = n - 1
 
-pro1Loop2 = Loop1 prog1BoolLoop2 (Seq prog1Atrib1Loop2 prog1Atrib2Loop2)
-pro1Loop1 = Loop1 prog1BoolLoop1 (Seq prog1Atrib1Loop1 (Seq prog1Atrib2Loop1 (Seq pro1Loop2 (Seq prog1Atrib3Loop1 prog1Atrib4Loop1))))
+fatLoop2 = Loop1 fatBoolLoop2 (Seq fatAtrib1Loop2 fatAtrib2Loop2)
+fatLoop1 = Loop1 fatBoolLoop1 (Seq fatAtrib1Loop1 (Seq fatAtrib2Loop1 (Seq fatLoop2 (Seq fatAtrib3Loop1 fatAtrib4Loop1))))
 
-programa1 = Seq prog1AtribN (Seq prog1AtribM (Seq prog1AtribR pro1Loop1))
+fatorial = Seq fatAtribN (Seq fatAtribM (Seq fatAtribR fatLoop1))
 
-resultProg1 = value (evalCommands programa1 memoryProg1) 'r'
+resultFat = value (evalCommands fatorial memoryFat) 'r'
+
+{-
+	Programa 02: Calculo daa multiplicacao com apenas soma
+
+	Em outra lingugem de programacao
+
+	  int n, m; // multiplicando e multiplicador da multiplicacao
+	  int r = 0;
+	 do{
+	     r = r+m;
+	     n = n - 1;
+	 }while(n > 0);
+
+-}
+
+memoryMult = initial
+
+multAtribN = Atrib 'n' (L 4) -- numero multiplicando
+multAtribM = Atrib 'm' (L 4) -- numero multiplicador
+multAtribR = Atrib 'r' (L 0) -- variavel para guardar o resultado
+multBoolLoop = Great (V 'n') (L 0) -- expressao n > 0 do loop
+multAtrib1Loop = Atrib 'r' (Add (V 'r') (V 'm')) -- r = r + m
+multAtrib2Loop = Atrib 'n' (Sub (V 'n') (L 1)) -- n = n - 1
+
+multLoop = Loop2 (Seq multAtrib1Loop multAtrib2Loop) multBoolLoop
+
+multiplicao = Seq multAtribN (Seq multAtribM (Seq multAtribR multLoop))
+
+resultMult = value (evalCommands multiplicao memoryMult) 'r'
