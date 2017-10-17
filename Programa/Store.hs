1904f3c385fcb90612b6de51bd13aed485d34b84
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-10-17 19:57:47 -0200

    Adicionada a função para retornar uma lista com a memoria mapeada para o programa

--- Programa/Store.hs
+++ Programa/Store.hs
@@ -13,7 +13,8 @@ module Store
    ( Store, 
      initial,     -- Store
      value,       -- Store -> Var -> Integer
-     update       -- Store -> Var -> Integer -> Store
+     update,       -- Store -> Var -> Integer -> Store
+     findSimTerms  -- Store -> Store -> Store
     ) where
 
 -- Var is the type of variables.                    
@@ -45,3 +46,34 @@ value (Store ((n,w):sto)) v
 update  :: Store -> Var -> Integer -> Store
 
 update (Store sto) v n = Store ((n,v):sto)
+
+
+memory = initial
+memory2 = initial
+
+sto2 = update memory 'x' 9
+sto3 = update sto2 'y' 10
+sto4 = update sto3 'z' 11
+sto5 = update sto4 'd' 12
+sto10 = update sto5 'd' 45
+
+sto6 = update memory2 'x' 13
+sto7 = update sto6 'y' 14
+sto8 = update sto7 'z' 15
+sto9 = update sto7 'd' 16
+
+-- metodo que retorna um store com os valores do primeiro store 
+-- que estao contidos no segundo store
+findSimTerms:: Store -> Store -> Store
+findSimTerms (Store [])  store  = (Store [])
+findSimTerms  store (Store []) =  store
+findSimTerms (Store ((n1,w1):sto)) sto2
+	|isterm w1 sto2 = update (findSimTerms (Store sto)  sto2) w1 n1
+	| otherwise = findSimTerms (Store sto) sto2
+
+	where 
+		isterm:: Char -> Store -> Bool
+		isterm _ (Store []) = False
+		isterm carct (Store ((n,w):sto))
+			| carct == w = True
+			|otherwise = isterm carct (Store sto)
